b24a84290e665a3826e32e851443c07fe6a3be55
Youngsun Noh <youngsunkr@gmail.com>
2015-06-25 00:28:18 +0900

    전문 처리 수정

--- src/main/java/com/jarvisef/test/DirectoryDefine.java
+++ src/main/java/com/jarvisef/test/DirectoryDefine.java
@@ -10,11 +10,14 @@ public class DirectoryDefine {
     public static final String DIRECTORY_HANACARD_RCV   	= base_Directory + "/HFG1000/RCV";
     public static final String DIRECTORY_HANABANK_RCV   	= base_Directory + "/HFG2000/RCV";
 
-    public static final String DIRECTORY_HANACARD_BACKUP_RCV   	= base_Directory + "/HFG1000/RCV/Backup";
-    public static final String DIRECTORY_HANABANK_BACKUP_RCV   	= base_Directory + "/HFG2000/RCV/Backup";
+    public static final String DIRECTORY_HANACARD_RCV_BACKUP   	= base_Directory + "/HFG1000/RCV/backup";
+    public static final String DIRECTORY_HANABANK_RCV_BACKUP   	= base_Directory + "/HFG2000/RCV/backup";
 
-    public static final String DIRECTORY_HANACARD_RND   	= base_Directory + "/HFG1000/SND";
-    public static final String DIRECTORY_HANABANK_RND   	= base_Directory + "/HFG2000/SND";
+    public static final String DIRECTORY_HANACARD_SND   	= base_Directory + "/HFG1000/SND";
+    public static final String DIRECTORY_HANABANK_SND   	= base_Directory + "/HFG2000/SND";
+
+    public static final String DIRECTORY_HANACARD_SND_BACKUP   	= base_Directory + "/HFG1000/SND/backup";
+    public static final String DIRECTORY_HANABANK_SND_BACKUP   	= base_Directory + "/HFG2000/SND/backup";
 }
 
 
